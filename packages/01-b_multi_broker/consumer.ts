4645536c3833ee88550f3f917d41860b38ada8a8
daitasu <ifctp.dai.monara@gmail.com>
2024-08-22 22:28:37 +0900

    feat: Add multi broker pattern

--- packages/01-b_multi_broker/consumer.ts
+++ packages/01-b_multi_broker/consumer.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "daitasu-consumer",
-  brokers: ["localhost:9092"],
+  brokers: ['localhost:29092', 'localhost:29093', 'localhost:29094'],
 });
 
 const consumer = kafka.consumer({ groupId: "test-group" });
@@ -12,10 +12,11 @@ const runConsumer = async () => {
   console.log("Consumer connected");
 
   // トピックの購読を開始
-  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+  await consumer.subscribe({ topic: "orders", fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
+      console.log('Topic:', topic);
       console.log({
         partition,
         offset: message.offset,
--- packages/01-b_multi_broker/producer.ts
+++ packages/01-b_multi_broker/producer.ts
@@ -2,7 +2,7 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "daitasu-producer",
-  brokers: ["localhost:9092"],
+  brokers: ['localhost:29092', 'localhost:29093', 'localhost:29094'],
 });
 
 const producer = kafka.producer();
@@ -13,8 +13,8 @@ const runProducer = async () => {
 
   // メッセージを送信
   await producer.send({
-    topic: "test-topic",
-    messages: [{ value: "こんにちは！" }, { value: "さようなら！" }],
+    topic: 'orders',
+    messages: [{ value: "鶏と野菜の黒酢あん" }, { value: "沖目鯛の醤油こうじ漬け炭火焼き" }, { value: "ピリ辛本格マーボードーフ" }],
   });
 
   console.log("Messages sent");
